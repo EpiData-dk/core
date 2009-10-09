07edf275698619675081c9d9e0a6b8ef913bd66d
torstenchr <github.com@bondechristiansen.dk>
2009-10-09 12:53:02 +0000

    VERSION BUMP: 0.2.1.99
    * Options set for release.

--- Utils/UEpiUtils.pas
+++ Utils/UEpiUtils.pas
@@ -281,7 +281,7 @@ begin
   CSI.PrgVersion.Major   := 0;
   CSI.PrgVersion.Minor   := 2;
   CSI.PrgVersion.Release := 1;
-  CSI.PrgVersion.Build   := 98;
+  CSI.PrgVersion.Build   := 99;
 
   CSI.CoreVersion := CoreVersion;
   // TODO -o Torsten : Get Subversion revision!
