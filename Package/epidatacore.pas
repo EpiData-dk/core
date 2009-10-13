9b79b27fc77536bcabb149585e90eb7e19ea8020
torstenchr <github.com@bondechristiansen.dk>
2009-10-13 13:29:31 +0000

    Commited first package build.

---
+++ Package/epidatacore.pas
@@ -0,0 +1,16 @@
+{ This file was automatically created by Lazarus. do not edit ! 
+  This source is only used to compile and install the package.
+ }
+
+unit epidatacore; 
+
+interface
+
+uses
+    UCheckFileCmds, UCheckFileIO, UCheckFileTypes, UDataFileTypes, 
+  UEpiDataFile, UValueLabels, UImportExport, UQesHandler, DCPcrypt, Rijndael, 
+  UDateUtils, UEpiLog, UEpiUtils, UStringUtils, UUtilTypes, UEpiDataGlobals;
+
+implementation
+
+end.
