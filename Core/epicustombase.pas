cada08dfbeaebdff31bec91fc4cf45fd7193965f
torstenchr <github.com@bondechristiansen.dk>
2010-04-08 17:49:28 +0000

    * Further implementation of custom classes.

--- Core/epicustombase.pas
+++ Core/epicustombase.pas
@@ -11,7 +11,8 @@ uses
 type
 
   { TEpiCustomBase }
-
+  TEpiEventGroup = (eegCustomBase);
+  TEpiCustomChangeEventType = (eceUpdate);
   TEpiChangeEvent = procedure(Sender: TObject; EventGroup: Word; EventType: Word; Data: Pointer) of object;
 
   TEpiCustomBase = class
@@ -28,6 +29,8 @@ type
     FOnChangeList: ^TEpiChangeEvent;
     FOnChangeListCount: Integer;
     FUpdateCount: Integer;
+  protected
+    procedure  DoChange(EventGroup: Word; EventType: Word; Data: Pointer);
   public
     // OnChange-hook methods
     procedure  BeginUpdate; virtual;
@@ -68,6 +71,8 @@ type
 
 implementation
 
+uses
+  StrUtils;
 { TEpiCustomBase }
 
 constructor TEpiCustomBase.Create(AOwner: TEpiCustomBase);
@@ -77,13 +82,14 @@ end;
 
 function TEpiCustomBase.Ins(Level: integer): string;
 begin
-
+  result := DupeString(' ', Level);
 end;
 
 procedure TEpiCustomBase.SaveToStream(St: TStream; Lvl: integer);
 var
   S: String;
 begin
+  // Base template - should be overridden in descendants.
   S := Ins(LvL) + '<' + ClassName + '>Not Implemented Yet</' + ClassName + '>' + LineEnding;
   St.Write(S[1], Length(S));
 end;
@@ -93,24 +99,57 @@ begin
   // Do nothing - should be overridden in descendants.
 end;
 
+procedure TEpiCustomBase.DoChange(EventGroup: word; EventType: word; Data: Pointer);
+var
+  i: Integer;
+begin
+  if FUpdateCount > 0 then exit;
+
+  for i := 0 to FOnChangeListCount - 1 do
+    FOnChangeList[i](Self, EventGroup, EventType, Data);
+end;
+
 procedure TEpiCustomBase.BeginUpdate;
 begin
-
+  Inc(FUpdateCount);
 end;
 
 procedure TEpiCustomBase.EndUpdate;
 begin
-
+  Dec(FUpdateCount);
+  if (FUpdateCount < 0) or (FUpdateCount > 0) then
+  begin
+    if (FUpdateCount < 0) then
+      FUpdateCount := 0;
+    exit;
+  end;
+  DoChange(word(eegCustomBase), word(eceUpdate), nil);
 end;
 
 procedure TEpiCustomBase.RegisterOnChangeHook(Event: TEpiChangeEvent);
 begin
-
+  Inc(FOnChangeListCount);
+  ReAllocMem(FOnChangeList, FOnChangeListCount * SizeOf(TEpiChangeEvent));
+  FOnChangeList[FOnChangeListCount-1] := Event
 end;
 
 procedure TEpiCustomBase.UnRegisterOnChangeHook(Event: TEpiChangeEvent);
+var
+  Idx: LongInt;
 begin
+  Idx := 0;
+  while Idx <= FOnChangeListCount -1 do
+  begin
+    if FOnChangeList[Idx] = Event then
+      break;
+    Inc(Idx)
+  end;
+  if Idx = FOnChangeListCount then exit;
 
+  dec(FOnChangeListCount);
+  if FOnChangeListCount > Idx then
+    System.Move(FOnChangeList[Idx+1],FOnChangeList[Idx],(FOnChangeListCount-Idx)*SizeOf(TEpiChangeEvent));
+  ReAllocMem(FOnChangeList, FOnChangeListCount*SizeOf(TEpiChangeEvent));
 end;
 
 { TEpiCustomList }
@@ -135,12 +174,13 @@ end;
 
 function TEpiCustomItem.GetName: string;
 begin
-
+  result := FName;
 end;
 
 procedure TEpiCustomItem.SetId(const AValue: string);
 begin
-
+  if FId = AValue then exit;
+  
 end;
 
 procedure TEpiCustomItem.SetName(const AValue: string);
