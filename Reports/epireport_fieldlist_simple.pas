61c3753b3f2bd81e288718158ba8441d73dbc568
torstenchr <github.com@bondechristiansen.dk>
2011-05-30 07:21:26 +0000

    * Added sorting on fieldlist.

--- Reports/epireport_fieldlist_simple.pas
+++ Reports/epireport_fieldlist_simple.pas
@@ -11,15 +11,18 @@ uses
 type
 
   { TEpiReportSimpleFieldList }
-
+  TEpiReportSimpleFieldListSortType = (stCreation, stFieldName, stEntryFlow);
   TEpiReportSimpleFieldList = class(TEpiReportBase)
   private
     FEpiDataFiles: TEpiDataFiles;
+    FSortType: TEpiReportSimpleFieldListSortType;
     procedure   PrintDatafile(Const DataFile: TEpiDataFile);
   public
-    constructor Create(const AEpiDocument: TEpiDocument); override;
+    constructor Create(const AEpiDocument: TEpiDocument;
+      Const ASortType: TEpiReportSimpleFieldListSortType = stCreation);
     procedure   RunReport; override;
     property    EpiDataFiles: TEpiDataFiles read FEpiDataFiles;
+    property    SortType: TEpiReportSimpleFieldListSortType read FSortType write FSortType;
   end;
 
   { TEpiReportSimpleFieldListHtml }
@@ -32,7 +35,8 @@ type
     function GetReportText: string; override;
   public
     constructor Create(const AEpiDocument: TEpiDocument;
-      Const CompleteHtml: boolean);
+      Const ASortType: TEpiReportSimpleFieldListSortType = stCreation;
+      Const CompleteHtml: boolean = true);
     destructor Destroy; override;
     procedure RunReport; override;
   end;
@@ -45,10 +49,71 @@ uses
 
 { TEpiReportSimpleFieldList }
 
-procedure TEpiReportSimpleFieldList.PrintDatafile(const DataFile: TEpiDataFile
-  );
+function FieldNameSort(Item1, Item2: Pointer): integer;
+var
+  Field1: TEpiField absolute Item1;
+  Field2: TEpiField absolute Item2;
+begin
+  result:= AnsiCompareStr(Field1.Name, Field2.Name);
+end;
+
+function EntryFlowSort(Item1, Item2: Pointer): integer;
+var
+  Field1: TEpiField absolute Item1;
+  Field2: TEpiField absolute Item2;
+  MainSection: TEpiSection;
+
+  function SectionTop(Const F: TEpiField): integer;
+  begin
+    result := F.Section.Top + F.Top;
+  end;
+  function SectionLeft(Const F: TEpiField): integer;
+  begin
+    result := F.Section.Left + F.Left;
+  end;
+
+begin
+  if (Field1.Section = Field2.Section) then
+  begin
+    Result := (Field1.Top - Field2.Top);
+    if Result = 0 then
+      Result := (Field1.Left - Field2.Left);
+    Exit;
+  end;
+
+  MainSection := Field1.DataFile.MainSection;
+
+  // Cross section comparison.
+  if (Field1.Section <> MainSection) and (Field2.Section <> MainSection) then
+  begin
+    result := SectionTop(Field1) - SectionTop(Field2);
+    if result = 0 then
+      Result := SectionLeft(Field1) - SectionLeft(Field2);
+    Exit;
+  end;
+
+  // Main <-> Section comparison
+  if (Field1.Section = MainSection) then
+  begin
+    result := Field1.Top - SectionTop(Field2);
+    if Result = 0 then
+      result := Field1.Left - SectionLeft(Field2);
+    exit;
+  end;
+
+  // Section <-> Main comparison
+  if (Field2.Section = MainSection) then
+  begin
+    result := SectionTop(Field1) - Field2.Top;
+    if Result = 0 then
+      result := SectionLeft(Field1) - Field2.Left;
+  end;
+end;
+
+procedure TEpiReportSimpleFieldList.PrintDatafile(const DataFile: TEpiDataFile);
 var
   i: Integer;
+  FieldList: TList;
 begin
   DoTableHeader(DataFile.Caption.Text, 4, DataFile.Fields.Count + 1);
   DoTableCell(0, 0, 'Name');
@@ -56,8 +121,17 @@ begin
   DoTableCell(2, 0, 'ValueLabel');
   DoTableCell(3, 0, 'Question');
 
-  for i := 0 to DataFile.Fields.Count - 1 do
-  with DataFile.Field[i] do
+
+  FieldList := TList.Create;
+  for i := 0 to DataFile.Fields.Count - 1 do FieldList.Add(DataFile.Field[i]);
+  case SortType of
+    stCreation:  ; // Do nothing...
+    stFieldName: FieldList.Sort(@FieldNameSort);
+    stEntryFlow: FieldList.Sort(@EntryFlowSort);
+  end;
+
+  for i := 0 to FieldList.Count - 1 do
+  with TEpiField(FieldList[i]) do
   begin
     DoTableCell(0, i+1, Name);
     DoTableCell(1, i+1, EpiTypeNames[FieldType]);
@@ -70,10 +144,12 @@ begin
   DoTableFooter('');
 end;
 
-constructor TEpiReportSimpleFieldList.Create(const AEpiDocument: TEpiDocument);
+constructor TEpiReportSimpleFieldList.Create(const AEpiDocument: TEpiDocument;
+  const ASortType: TEpiReportSimpleFieldListSortType);
 begin
   inherited Create(AEpiDocument);
   FEpiDataFiles := AEpiDocument.DataFiles;
+  FSortType := ASortType;
 end;
 
 procedure TEpiReportSimpleFieldList.RunReport;
@@ -94,9 +170,11 @@ begin
 end;
 
 constructor TEpiReportSimpleFieldListHtml.Create(
-  const AEpiDocument: TEpiDocument; const CompleteHtml: boolean);
+  const AEpiDocument: TEpiDocument;
+  const ASortType: TEpiReportSimpleFieldListSortType;
+  const CompleteHtml: boolean);
 begin
-  inherited Create(AEpiDocument);
+  inherited Create(AEpiDocument, ASortType);
   FHtmlGenerator := TEpiReportHTMLGenerator.Create(Self);
   FCompleteHtml := CompleteHtml;
 end;
