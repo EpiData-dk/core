d51c1c18e9e046c4bd2510518bc6a7344f0ec82b
torstenchr <github.com@bondechristiansen.dk>
2014-01-10 10:00:29 +0000

    * Export/Import type: Added string constants to enum types. * Finished implementing Visitors for ExportSettings in Reports.

--- Reports/epireport_report_exportsettings_visitors.pas
+++ Reports/epireport_report_exportsettings_visitors.pas
@@ -15,6 +15,7 @@ type
   private
     FReportGenerator: TEpiReportGeneratorBase;
     FRowIndex: integer;
+    procedure DoTableCell(Const Col: Integer; Const Text: string);
   public
     constructor Create; override;
     procedure Visit(const ExportSetting: TEpiExportSetting); override; overload;
@@ -73,9 +74,20 @@ type
 implementation
 
 uses
-  epieximtypes;
+  epieximtypes, epireport_types;
 
 { TSettingsTableOutputVisitor }
+
+procedure TSettingsTableOutputVisitor.DoTableCell(const Col: Integer;
+  const Text: string);
+var
+  Adjust: TEpiReportGeneratorTableCellAdjustment;
+begin
+  Adjust := tcaAutoAdjust;
+  if Col = 1 then
+    Adjust := tcaRightAdjust;
+  FReportGenerator.TableCell(Text, Col, RowIndex, Adjust);
+end;
 
 constructor TSettingsTableOutputVisitor.Create;
 begin
@@ -85,101 +97,132 @@ end;
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiExportSetting);
 begin
-  FReportGenerator.TableCell(
-    'From',
-    0,
-    RowIndex);
-
-  FReportGenerator.TableCell(
-    IntToStr(ExportSetting.FromRecord),
-    1,
-    RowIndex);
-
+  DoTableCell(0, 'From');
+  DoTableCell(1, IntToStr(ExportSetting.FromRecord));
   Inc(FRowIndex);
 
-  FReportGenerator.TableCell(
-    'To',
-    0,
-    RowIndex);
-
-  FReportGenerator.TableCell(
-    IntToStr(ExportSetting.ToRecord),
-    1,
-    RowIndex);
-
+  DoTableCell(0, 'To');
+  DoTableCell(1, IntToStr(ExportSetting.ToRecord));
   Inc(FRowIndex);
 
-  FReportGenerator.TableCell(
-    'Encoding',
-    0,
-    RowIndex);
-
-  FReportGenerator.TableCell(
-    EpiEncodingToString[ExportSetting.Encoding],
-    1,
-    RowIndex);
-
+  DoTableCell(0, 'Encoding');
+  DoTableCell(1, EpiEncodingToString[ExportSetting.Encoding]);
   Inc(FRowIndex);
 
-  FReportGenerator.TableCell(
-    'Export Deleted',
-    0,
-    RowIndex);
-
-  FReportGenerator.TableCell(
-    BoolToStr(ExportSetting.ExportDeleted, true),
-    1,
-    RowIndex);
-
+  DoTableCell(0, 'Export Deleted');
+  DoTableCell(1, BoolToStr(ExportSetting.ExportDeleted, true));
   Inc(FRowIndex);
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiCustomValueLabelExportSetting);
 begin
-
+  DoTableCell(0, 'Export Valuelabels');
+  DoTableCell(1, BoolToStr(ExportSetting.ExportValueLabels, true));
+  Inc(FRowIndex);
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiStataExportSetting);
 begin
+  DoTableCell(0, 'Field name case');
+  DoTableCell(1, EpiFieldNamingCaseToString[ExportSetting.FieldNameCase]);
+  Inc(FRowIndex);
 
+  DoTableCell(0, 'Stata Version');
+  DoTableCell(1, EpiStataVersionToString(ExportSetting.Version));
+  Inc(FRowIndex);
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiCSVExportSetting);
 begin
+  DoTableCell(0, 'Field Separator');
+  DoTableCell(1, ExportSetting.FieldSeparator);
+  Inc(FRowIndex);
 
+  DoTableCell(0, 'Date Separator');
+  DoTableCell(1, ExportSetting.DateSeparator);
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'Time Separator');
+  DoTableCell(1, ExportSetting.TimeSeparator);
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'Decimal Separtor');
+  DoTableCell(1, ExportSetting.DecimalSeparator);
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'New Line Style');
+  DoTableCell(1, '');
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'Fixed Format');
+  DoTableCell(1, BoolToStr(ExportSetting.FixedFormat, true));
+  Inc(FRowIndex);
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiCustomTextExportSettings);
 begin
+  DoTableCell(0, 'Byte Order Mark');
+  DoTableCell(1, BoolToStr(ExportSetting.ByteOrderMark, true));
+  Inc(FRowIndex);
 
+  DoTableCell(0, 'Export Field Names (first row)');
+  DoTableCell(1, BoolToStr(ExportSetting.ExportFieldNames, true));
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'Quote Character');
+  DoTableCell(1, ExportSetting.QuoteChar);
+  Inc(FRowIndex);
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiDDIExportSetting);
 begin
+  DoTableCell(0, 'Remove Value Labels indicating missing');
+  DoTableCell(1, BoolToStr(ExportSetting.RemoveMissingVL, true));
+  Inc(FRowIndex);
 
+  DoTableCell(0, 'Use <UserID> for filter');
+  DoTableCell(1, BoolToStr(ExportSetting.FilterTagIsUserId, true));
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'Use section caption as <Question Text> for subquestions');
+  DoTableCell(1, BoolToStr(ExportSetting.SectionCaptionIsQText, true));
+  Inc(FRowIndex);
+
+  DoTableCell(0, 'Rename variables');
+  DoTableCell(1, BoolToStr(ExportSetting.RenameVariablesPrefix <> '', true));
+  Inc(FRowIndex);
+
+  if ExportSetting.RenameVariablesPrefix <> '' then
+    begin
+      DoTableCell(0, 'Variable prefix');
+      DoTableCell(1, ExportSetting.RenameVariablesPrefix);
+      Inc(FRowIndex);
+    end;
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiEPXExportSetting);
 begin
-
+  // Do nothing
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiSASExportSetting);
 begin
-
+  // Do nothing
 end;
 
 procedure TSettingsTableOutputVisitor.Visit(
   const ExportSetting: TEpiSPSSExportSetting);
 begin
-
+  DoTableCell(0, 'Delimiter');
+  DoTableCell(1, ExportSetting.Delimiter);
+  Inc(FRowIndex);
 end;
 
 { TSetttingCountVisitor }
@@ -232,7 +275,10 @@ end;
 procedure TSettingCountVisitor.Visit(const ExportSetting: TEpiDDIExportSetting);
 begin
   DebugStr('TEpiDDIExportSetting');
-  inc(FCount, 8);
+  inc(FCount, 4);
+
+  if ExportSetting.RenameVariablesPrefix <> '' then
+    inc(FCount);
 end;
 
 procedure TSettingCountVisitor.Visit(const ExportSetting: TEpiEPXExportSetting);
