c84ed6cb6ea5fe3c8aa480d3b9026b4cd53aa122
Jamie <charlesknightsbridge@gmail.com>
2023-02-16 12:25:17 -0500

    fixes references to epi_osxlocale for Lazarus

--- epidatacore.pas
+++ epidatacore.pas
@@ -30,7 +30,7 @@ uses
   epiimport_stata, epitools_statusbarparser, epi_rsa, epilogger, 
   episecuritylog, epicustomlist_helper, episervice_asynchandler, 
   epiimportsettings, epitools_export_seclog, epireport_report_userlist, 
-  epitools_archieve, epidocument_helper, StrHashMap, pair;
+  epitools_archieve, epidocument_helper, StrHashMap, pair, epi_osxlocale;
 
 implementation
 
